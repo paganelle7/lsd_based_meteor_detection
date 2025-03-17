80365cf54a55bc1ddb1956b4338d506eba130f49
paganelle7 <paganelle7@mail.ru>
2025-03-17 13:13:59 +0300

    Update meteor_segment_detection.py
    Add end points viewing

--- meteor_segment_detection.py
+++ meteor_segment_detection.py
@@ -18,6 +18,7 @@ def unique_neighbors(grad_x,n=8):
     ind_arr =torch.arange(shape, device=device).int()
     ind0_arr =torch.arange(shape, device=device).int()
     weights = torch.ones(shape, device=device).int()
+    ##ending = torch.ones(shape, device=device).int()
     for i in range(1,n):
         condition = (grad_x[i:]==grad_x[:-i])
         ind_arr[i:] = torch.minimum(ind0_arr[i:] - i*condition, ind_arr[i:])
@@ -25,9 +26,10 @@ def unique_neighbors(grad_x,n=8):
         
         weights[:-i] += 1*condition*torch.sign(weights[:-i])
         
+    
     return torch.where(weights>0, weights,0), ind_arr
 
-def one_grad_groop(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, threshold=0.9):
+def one_grad_groop(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end, threshold=0.9, weight_un = 8):
     shape = main_grad_x.shape;    h,w = main_grad_x.shape;    device = main_grad_x.device
     last_grad_x, last_grad_y = main_grad_x[0], main_grad_y[0]
     last_weight = weight_of_groops[0]#torch.ones((w)); 
@@ -58,11 +60,13 @@ def one_grad_groop(main_grad_x, main_grad_y, weight_of_groops, x_center, y_cente
             #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!WRITING
             x_center[i+1] = last_center_x
             y_center[i+1] = last_center_y
+
             #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!          
             #merge coords
             
-            more_weights, inverse_index = unique_neighbors(last_grad_x, n=6)
+            more_weights, inverse_index = unique_neighbors(last_grad_x, n=weight_un)
             # print(last_weight, more_weights)
+            #ending_index = torch.arange(shape, device=device)+more_weights//2
             keys = torch.sign(more_weights)
             groop_grad_x = last_grad_x*keys; groop_grad_y = last_grad_y*keys
             groop_weights = last_weight*keys
@@ -88,15 +92,20 @@ def one_grad_groop(main_grad_x, main_grad_y, weight_of_groops, x_center, y_cente
             main_grad_x[last_center_x[keys!=0], last_center_y[keys!=0]] = groop_grad_x[keys!=0]
             main_grad_y[last_center_x[keys!=0], last_center_y[keys!=0]] = groop_grad_y[keys!=0]
             weight_of_groops[last_center_x[keys!=0], last_center_y[keys!=0]] = groop_weights[keys!=0]
+            
+            centr_update = more_weights//2 + torch.arange(more_weights.shape[0], device=device,dtype=torch.int32)
+            x_end[last_center_x[keys!=0], last_center_y[keys!=0]] = i+1
+            y_end[last_center_x[keys!=0], last_center_y[keys!=0]] = centr_update[keys!=0]
+            
             #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             # print(t1-t0,t2-t1,t3-t2,t4-t3)
 ##################################################################################################
             # if i > 0:
             #     break
     weight_of_groops[-1]=0
-    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center
+    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end
 
-def contraction(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,  batch_size):
+def contraction(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end,  batch_size):
     # applying bootom
     shape = main_grad_x.shape
     x_axis = shape[0];
@@ -104,6 +113,7 @@ def contraction(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, 
     zeros = torch.zeros((x_axis,1), device=x_center.device)
     main_grad_x = torch.cat((main_grad_x,zeros),dim=1);    main_grad_y = torch.cat((main_grad_y,zeros),dim=1)
     x_center = torch.cat((x_center,zeros.int()),dim=1);    y_center = torch.cat((y_center,zeros.int()),dim=1)
+    x_end = torch.cat((x_end,zeros.int()),dim=1);    y_end = torch.cat((y_end,zeros.int()),dim=1)
     
     weight_of_groops = torch.cat((weight_of_groops,zeros.int()),dim=1)
     #applying zeros
@@ -114,61 +124,78 @@ def contraction(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, 
     
     main_grad_x = torch.cat((main_grad_x,zeros));    main_grad_y = torch.cat((main_grad_y,zeros))
     x_center = torch.cat((x_center,zeros.int()));    y_center = torch.cat((y_center,zeros.int()))
+    x_end = torch.cat((x_end,zeros.int()));    y_end = torch.cat((y_end,zeros.int()))
     weight_of_groops = torch.cat((weight_of_groops,zeros.int()))
     #index changing
     y_center = y_center + x_center//batch_size * y_axis
     x_center %= batch_size
+    y_end = y_end + x_end//batch_size * y_axis
+    x_end %= batch_size
     #reformat
     main_grad_x = torch.cat(main_grad_x.split(batch_size, dim=0),dim=1);    main_grad_y = torch.cat(main_grad_y.split(batch_size, dim=0),dim=1)
     x_center = torch.cat(x_center.split(batch_size, dim=0),dim=1);     y_center = torch.cat(y_center.split(batch_size, dim=0),dim=1)
+    x_end = torch.cat(x_end.split(batch_size, dim=0),dim=1);     y_end = torch.cat(y_end.split(batch_size, dim=0),dim=1)
     weight_of_groops = torch.cat(weight_of_groops.split(batch_size, dim=0),dim=1)
-    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_axis, x_need, y_axis
+    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end, x_axis, x_need, y_axis
           
-def extention(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,   x_axis, x_need, y_axis, batch_size):
+def extention(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end,  x_axis, x_need, y_axis, batch_size):
     #index returning
     y_center = y_center - torch.arange(y_center.shape[1],device=y_center.device)//y_axis*y_axis
     x_center = x_center + torch.arange(y_center.shape[1],device=y_center.device)//y_axis*batch_size
+    y_end = y_end - torch.arange(y_end.shape[1],device=y_end.device)//y_axis*y_axis
+    x_end = x_end + torch.arange(y_end.shape[1],device=y_end.device)//y_axis*batch_size
     #returning origin shape and clear exscess zeros
     main_grad_x = torch.cat(main_grad_x.split(y_axis, dim=1),dim=0)[:x_axis,:-1];    main_grad_y = torch.cat(main_grad_y.split(y_axis, dim=1),dim=0)[:x_axis,:-1]
     x_center = torch.cat(x_center.split(y_axis, dim=1),dim=0)[:x_axis,:-1];     y_center = torch.cat(y_center.split(y_axis, dim=1),dim=0)[:x_axis,:-1]
+    x_end = torch.cat(x_end.split(y_axis, dim=1),dim=0)[:x_axis,:-1];     y_end = torch.cat(y_end.split(y_axis, dim=1),dim=0)[:x_axis,:-1]
+
     weight_of_groops = torch.cat(weight_of_groops.split(y_axis, dim=1),dim=0)[:x_axis,:-1]
 
-    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center
+    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end
     
-def lsd_optimysed(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, threshold=0.7, batch_size=5):
-        # create rotate array and and change x y placec
-    m2x,m2y,w2,y2,x2 = torch.transpose(main_grad_x.clone(),1,0),torch.transpose(main_grad_y.clone(),1,0),torch.transpose(weight_of_groops.clone(),1,0),torch.transpose(x_center.clone(),1,0),torch.transpose(y_center.clone(),1,0)
-    m2x,m2y,w2,x2,y2,x2_axis, x2_need, y2_axis =  contraction( m2x,m2y,w2,x2,y2 , batch_size=batch_size)
+def lsd_optimysed(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end, threshold=0.7, batch_size=5, weight_un=8):
+    # create rotate array and and change x y placec
+    m2x,m2y,w2,y2,x2, ye2, xe2 = torch.transpose(main_grad_x.clone(),1,0),torch.transpose(main_grad_y.clone(),1,0),torch.transpose(weight_of_groops.clone(),1,0),torch.transpose(x_center.clone(),1,0),torch.transpose(y_center.clone(),1,0),torch.transpose(x_end.clone(),1,0),torch.transpose(y_end.clone(),1,0)
+    m2x,m2y,w2,x2,y2,xe2,ye2, x2_axis, x2_need, y2_axis =  contraction( m2x,m2y,w2,x2,y2 ,xe2, ye2, batch_size=batch_size)
         # main_grad_x, main_grad_y, weight_of_groops, x_center, y_center = m2x,m2y,w2,x2,y2
         
     
-    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_axis, x_need, y_axis = contraction(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,batch_size=batch_size)
+    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,x_end, y_end, x_axis, x_need, y_axis = contraction(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end, batch_size=batch_size)
     edge = main_grad_x.shape[1]
         #concantenation
 
-    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center = torch.cat((main_grad_x, m2x),dim=1), torch.cat((main_grad_y, m2y),dim=1),torch.cat((weight_of_groops, w2),dim=1),torch.cat((x_center, x2),dim=1),torch.cat((y_center, y2+edge),dim=1),
-    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center = one_grad_groop(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, threshold=threshold)
-        # disassembling arrays
-    m2x,m2y,w2,x2,y2 = main_grad_x.clone()[:,edge:], main_grad_y.clone()[:,edge:], weight_of_groops.clone()[:,edge:], x_center.clone()[:,edge:], y_center.clone()[:,edge:]-edge
+    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,x_end, y_end = torch.cat((main_grad_x, m2x),dim=1), torch.cat((main_grad_y, m2y),dim=1),torch.cat((weight_of_groops, w2),dim=1),torch.cat((x_center, x2),dim=1),torch.cat((y_center, y2+edge),dim=1),torch.cat((x_end, x2),dim=1), torch.cat((y_end, y2+edge),dim=1)
+    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end = one_grad_groop(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end, threshold=threshold,weight_un=weight_un)
+    #     # disassembling arrays 
+    m2x,m2y,w2,x2,y2, xe2, ye2 = main_grad_x.clone()[:,edge:], main_grad_y.clone()[:,edge:], weight_of_groops.clone()[:,edge:], x_center.clone()[:,edge:], y_center.clone()[:,edge:]-edge,x_end.clone()[:,edge:], y_end.clone()[:,edge:]-edge
 
-    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center = main_grad_x[:,:edge], main_grad_y[:,:edge], weight_of_groops[:,:edge], x_center[:,:edge], y_center[:,:edge]
-        # unpack arrays
-    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center = extention(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,   x_axis, x_need, y_axis, batch_size=batch_size)
-    m2x,m2y,w2,x2,y2 =  extention( m2x,m2y,w2,x2,y2,x2_axis, x2_need, y2_axis , batch_size=batch_size)
+    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end = main_grad_x[:,:edge], main_grad_y[:,:edge], weight_of_groops[:,:edge], x_center[:,:edge], y_center[:,:edge], x_end[:,:edge], y_end[:,:edge]
+#         # unpack arrays
+    # main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end = m2x,m2y,w2,x2,y2, xe2, ye2
+        
+    main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,x_end, y_end = extention(main_grad_x, main_grad_y, weight_of_groops, x_center, y_center, x_end, y_end,  x_axis, x_need, y_axis, batch_size=batch_size)
+    m2x,m2y,w2,x2,y2,xe2, ye2 =  extention( m2x,m2y,w2,x2,y2,xe2, ye2,x2_axis, x2_need, y2_axis , batch_size=batch_size)
     y_center = torch.maximum(y_center, torch.zeros(y_center.shape,device=device).int())
     y2 = torch.maximum(y2, torch.zeros(y2.shape,device=device).int())
-    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,m2x,m2y,w2,x2,y2
+    y_end = torch.maximum(y_end, torch.zeros(y_end.shape,device=device).int())
+    ye2 = torch.maximum(ye2, torch.zeros(ye2.shape,device=device).int())
+    return main_grad_x, main_grad_y, weight_of_groops, x_center, y_center,x_end, y_end, m2x,m2y,w2,x2,y2,xe2, ye2
 
 
-def lsd_meteor_on_image(image, threshold=0.9,batch_size=30):
+def lsd_meteor_on_image(image, threshold=0.9,batch_size=30,weight_un=8):
     grad_x, grad_y = grad_ima(image)
     shape = grad_x.shape;    h,w = grad_x.shape;    device = grad_x.device
     weight_of_groops = torch.ones(shape,device=device)
     x_center = (torch.ones(shape, device=device).int() )*torch.arange(h, device=device)[:,None].int()
-    y_center = (torch.ones(shape, device=device).int())*torch.arange(w, device=device)[None,:].int()
-    hgrad_x, hgrad_y, hweight_of_groops, hx_center, hy_center, m2x,m2y,w2,x2,y2 = lsd_optimysed(grad_x.clone(), grad_y.clone(), weight_of_groops.clone(), x_center.clone(), y_center.clone(), threshold=threshold, batch_size=batch_size)
+    y_center = (torch.ones(shape, device=device).int() )*torch.arange(w, device=device)[None,:].int()
+    
+    x_end = (torch.ones(shape, device=device).int() )*torch.arange(h, device=device)[:,None].int()
+    y_end = (torch.ones(shape, device=device).int())*torch.arange(w, device=device)[None,:].int()
+
+    
+    hgrad_x, hgrad_y, hweight_of_groops, hx_center, hy_center,x_end, y_end, m2x,m2y,w2,x2,y2,xe2, ye2 = lsd_optimysed(grad_x.clone(), grad_y.clone(), weight_of_groops.clone(), x_center.clone(), y_center.clone(), x_end.clone(), y_end.clone(), threshold=threshold, batch_size=batch_size,weight_un=weight_un)
     # 
     # wgrad_x, wgrad_y, wweight_of_groops, wx_center, wy_center = lsd_optimysed(grad_x.clone(), grad_y.clone(), weight_of_groops.clone(), x_center.clone(), y_center.clone(), threshold=0.7)
 
     # get_meteors()
-    return hweight_of_groops,hx_center, hy_center, hgrad_x, hgrad_y, w2,x2,y2,m2x,m2y
+    return hweight_of_groops,hx_center, hy_center, x_end ,y_end, hgrad_x,  hgrad_y, w2,x2,y2,xe2, ye2,m2x,m2y
